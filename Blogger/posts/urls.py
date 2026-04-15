05bf4790775dd2f4c624c5157cb7985e0c641ee5
FadhelAlmalki <almalkifadhel@gmail.com>
2026-04-16 00:56:31 +0300

    Added the lab changes as per 15th of April

--- Blogger/posts/urls.py
+++ Blogger/posts/urls.py
@@ -6,5 +6,7 @@ app_name = 'posts'
 
 urlpatterns =[
     path('create/', views.create_post_view, name='create_post_view'),
-    path('<int:post_id>/', views.post_detail_view, name='post_detail_view'),
+    path('detail/<int:post_id>/', views.post_detail_view, name='post_detail_view'),
+    path('update/<int:post_id>/', views.update_post_view, name='update_post_view'),
+    path('delete/<int:post_id>/', views.delete_post_view, name='delete_post_view'),
 ]
--- Blogger/posts/views.py
+++ Blogger/posts/views.py
@@ -2,23 +2,45 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpRequest
 from .models import Post
 
+# Create posts
 def create_post_view(request: HttpRequest):
+    
     if request.method == 'POST':
-        title = request.POST.get("title", "").strip()
-        content = request.POST.get("content", "").strip()
-
-        if title and content:
-            new_post = Post(
-                title=title,
-                content=content,
-                is_published=True,
-            )
-            new_post.save()
-            return redirect('main:home_view')
+        new_post = Post(title=request.POST["title"], content=request.POST["content"])
+        new_post.save()
+        return redirect("main:home_view")
 
     return render(request, "posts/create.html")
 
-
+# Read posts
 def post_detail_view(request: HttpRequest, post_id: int):
-    post = get_object_or_404(Post, id=post_id, is_published=True)
+
+    post = Post.objects.get(pk=post_id)
+
     return render(request, "posts/detail.html", {"post": post})
+
+# Update posts
+def update_post_view(request: HttpRequest, post_id: int):
+
+    post = Post.objects.get(pk=post_id)
+
+    if request.method == 'POST':
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        if "poster" in request.FILES: post.poster = request.FILES["poster"]
+        post.save()
+
+        return redirect("posts:post_detail_view", post_id=post.id)
+
+    return render(request, "posts/post_update.html", {"post": post})
+
+# Delete posts
+def delete_post_view(request: HttpRequest, post_id: int):
+
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+
+    return redirect("main:home_view")
+
+
+
